733518fed9181479105681c10f14cb3a46497bd0
Dmitri <datitov2009@yandex.ru>
2023-01-29 12:09:44 +0300

    add homework3 folder

---
+++ Homework3/FamilyTree/FamilyMemeber.java
@@ -0,0 +1,28 @@
+package FamilyTree;
+
+import java.util.List;
+
+/**
+ * Класс расширяющий Human, наследует всё из родительского класса
+ */
+public class FamilyMemeber extends Human {
+
+    public FamilyMemeber(String fio) {
+        super(fio);
+    }
+
+    public FamilyMemeber(String fio, I_FamilyTree parent) {
+        super(fio, parent);
+    }
+
+    public FamilyMemeber(String fio, I_FamilyTree parent1, I_FamilyTree parent2) {
+        super(fio, parent1, parent2);
+    }
+
+    @Override
+    public String toString() {
+        return this.getFio();
+    }
+
+}
+
---
+++ Homework3/FamilyTree/Programm.java
@@ -0,0 +1,22 @@
+package FamilyTree;
+
+import java.util.ArrayList;
+
+public class Programm {
+    public static void main(String[] args) {
+        FamilyMemeber mem1 = new FamilyMemeber("Ivanov I.I.");
+        FamilyMemeber mem2 = new FamilyMemeber("Ivanova E.P.");
+        FamilyMemeber mem3 = new FamilyMemeber("Ivanov O.I.", mem1, mem2);
+        FamilyMemeber mem4 = new FamilyMemeber("Ivanova A.I.", mem1, mem2);
+        mem1.setSpouse(mem2);
+        mem2.setSpouse(mem1);
+        mem1.addChild(mem3);
+        mem1.addChild(mem4);
+        mem2.addChild(mem3);
+        mem2.addChild(mem4);
+
+        System.out.println(mem3);
+
+
+    }
+}
