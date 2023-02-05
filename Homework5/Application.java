8cf46cd90cafa52f21017b4c83508ed037dc54f3
Dmitri <datitov2009@yandex.ru>
2023-02-05 20:06:27 +0300

    add Homework5 folder

---
+++ Homework5/Application.java
@@ -0,0 +1,10 @@
+import Control.*;
+import Person.*;
+import View.*;
+public class Application {
+    public static void main(String[] args) {
+        MainControl app = new MainControl();
+        app.runApplication();
+    }
+}
+
